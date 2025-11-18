95d116c26ab24030ce0e0113803521f6dadd455d
08SjoAnt <anton.sjogren@skola.taby.se>
2025-11-18 10:47:20 +0100

    Lade till fler skatter

--- skatter.py
+++ skatter.py
@@ -1,20 +1,49 @@
+from random import randint
 class skatt:
-    def __init__(skatt, namn, kvalitet, kpmod, stymod, nivamod, beskrivning):
+    kvalitet = 1
+    def __init__(skatt, namn, kpmod, stymod, nivamod, beskrivning):
         skatt.namn = namn
-        skatt.kvalitet = kvalitet
         skatt.kpmod = kpmod
         skatt.stymod = stymod
         skatt.nivamod = nivamod
         skatt.beskrivning = beskrivning
 
-# skatt('NAMN', kvalitet, kp modifier, sty modifier, niva modifier, '"Beskrivning"')
+# skatt('NAMN', kp modifier, sty modifier, niva modifier, '"Beskrivning"')
 #Kvalitet 1 föremål
-teleskop = skatt('Teleskop', 1, 0, 0, 0, '"Ökad sikt"')
-spade = skatt('Spade', 1, 0, 1, 0, '"Gräva gräva hål!"')
+k1 = [
+skatt('Teleskop', 0, 0, 0, '"Ökad sikt"'),
+skatt('Spade', 0, 1, 0, '"Gräva gräva hål!"')
+]
+for i in k1:
+    print(i.namn, 'Kvalitet:', i.kvalitet)
+    print(i.beskrivning)
 #Kvalitet 2 föremål
-
+k2 = [
+skatt('Raket i en burk', -1, 2, 0, '"Jag förstår inte riktigt hur man fick in den..."'),
+skatt('Pandoras ask', randint(-2, 4), randint(-5, 5), randint(0, 1), '"Slumpmässig egenskapsförändring"')
+]
+for i in k2:
+    i.kvalitet = 2 #Ger alla items i listan k2 kvalitetvärde 2
+    print(i.namn, 'Kvalitet:', i.kvalitet)
+    print(i.beskrivning)
+    if i.namn == 'Pandoras ask':
+        print('sty', i.stymod)
+        print('kp', i.kpmod)
+        print('nivå', i.nivamod)
 #Kvalitet 3 föremål
-
+k3 = [
+skatt('Sten', -3, 4, 0, '"Hur kan en sten gråta?!"')
+]
+for i in k3:
+    i.kvalitet = 3 #Ger alla items i listan k3 kvalitetvärde 3
+    print(i.namn, 'Kvalitet:', i.kvalitet)
+    print(i.beskrivning)
 #Kvalitet 4 föremål
-holy_obliterator = skatt('Helig utplånare', 4, 0, 10, 1, '"Hellre detta än "Universumsförstörare""')
-universe_destroyer = skatt('Universumförstare', 4, 10, 0, 1, '"Hellre detta än "Helig utplånare""')
+k4 = [
+skatt('Helig utplånare', 0, 10, 1, '"Hellre detta än Universumsförstörare"'),
+skatt('Universumförstare', 10, 0, 1, '"Hellre detta än Helig utplånare"')
+]
+for i in k4:
+    i.kvalitet = 4 #Ger alla items i listan k4 kvalitetvärde 4
+    print(i.namn, 'Kvalitet:', i.kvalitet)
+    print(i.beskrivning)
